8705d99579e005c99e83798e4c391e9128a281fc
regulus <regulus@ThinkPad-X230.(none)>
2013-02-28 22:43:39 +0800

    update

---
+++ c_lang/time/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int i=0;
+
+	int seconds=time((time_t*)NULL);
+	srand((int)time(0));
+	
+	printf("%d\n",seconds);
+
+	
+	for(i=0;i<10;i++)
+	{
+		printf("%d\n",rand()%100);
+	}
+}
