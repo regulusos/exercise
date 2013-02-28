fdfb714f52982a2c185c093723df87f8c374197a
panhaitao <regulus_cn@163.com>
2013-02-28 21:32:01 +0800

    add dir

---
+++ c_lang/base/point/point.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int mian()
+{
+	int p=3;
+	int *p1;
+	int **p2;
+	p1=&p;
+	p2=&p1;
+
+	printf("addr\tvalue\t")
+	printf("%p\t%d\t\n",&p,p);
+	printf("%p\t%d\t\n",&p1,p1);
+	printf("%p\t%d\t\n",&p2,p2);
+}
