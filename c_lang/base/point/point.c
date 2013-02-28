19e5c363cf33411f1901bd3b1cb42a6c67da16eb
regulus <regulus@ThinkPad-X230.(none)>
2013-02-28 22:39:21 +0800

    update

--- c_lang/base/point/point.c
+++ c_lang/base/point/point.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int mian()
+int main()
 {
 	int p=3;
 	int *p1;
@@ -8,8 +8,8 @@ int mian()
 	p1=&p;
 	p2=&p1;
 
-	printf("addr\tvalue\t\n")
-	printf("%p\t%d\t\n",&p,p);
-	printf("%p\t%d\t\n",&p1,p1);
-	printf("%p\t%d\t\n",&p2,p2);
+	printf("name\taddr\t\tvalue\t\n");
+	printf("p \t%p\t%d\t\n",&p,p);
+	printf("p1\t%p\t%p\t\n",&p1,p1);
+	printf("p2\t%p\t%p\t\n",&p2,p2);
 }
