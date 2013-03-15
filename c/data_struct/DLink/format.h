f6489d90a8e15892af25e9656a5a84dfc5352e53
regulus <regulus@regulus-desktop.(none)>
2013-03-15 17:09:36 +0800

    update

---
+++ c/data_struct/DLink/format.h
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct _D_LINK_LIST
+{
+	struct _D_LINK_LIST *prev;
+	struct _D_LINK_LIST *next;
+	int   data;
+}D_LINK_LIST;
+
+int print(D_LINK_LIST* thiz);
+
+int dlist_init(D_LINK_LIST *thiz);
+int dlist_add(D_LINK_LIST *thiz);
+int dlist_add(D_LINK_LIST *thiz);
+
+int dlist_print(D_LINK_LIST* thiz); //打印列表内容
+int dlist_find_max(D_LINK_LIST *thiz);
+int dlist_sum(D_LINK_LIST *thiz);
+
+
+
